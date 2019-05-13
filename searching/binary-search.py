2dc38b9708a8ee2fe3369fe0d8d6b930fa7d1393
hkv <brosgiant@gmail.com>
2019-05-13 22:52:28 +0530

    13may2019 late night

--- searching/binary-search.py
+++ searching/binary-search.py
@@ -1,5 +1,10 @@
+
+''' we can use both recursion and iteration to find solution'''
+
+# BINARY SEARCH USING RECURSION
+
 def binary_search(lis, f, l, target):
-    mid=int(l-(l-f)/2)
+    mid=int((l+f)/2)
     if l>=f:
         if lis[mid]==target:
             return mid
@@ -18,3 +23,27 @@ if result!=-1:
     print(f'found at {result+1}')
 else:
     print('not found')
+
+
+# BINARY SEARCH USING ITERATION
+
+def bin_se_ite(lis,f,l,target):
+    while f<=l:
+        mid=(l+f)//2
+        if lis[mid]==target:
+            return mid
+        elif lis[mid]>target:
+            l=mid-1
+        else:
+            f=mid+1
+    else:
+        return -1
+
+lis=[2,5,6,9,10,12,14,15,16,17,18,111]
+result=bin_se_ite(lis, 0, len(lis)-1, 111)
+if result!=-1:
+    print(f'found at {result+1}')
+else:
+    print('not found')
+
+
